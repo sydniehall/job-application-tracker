e46a4eb414eb941f876053e11d224821cbe4e7af
sydniehall <sydhall64@gmail.com>
2026-06-10 19:08:52 -0500

    migrate database.py from sqlalchemy to sqlmodel

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel, create_engine, Session
 
 DATABASE_URL = "sqlite:///./tracker.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-        
+    with Session(engine) as session:
+        yield session
